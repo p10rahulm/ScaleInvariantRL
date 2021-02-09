cd5835f61b332b15ddd7ed866c30ea5828aee4a7
Rahul Madhavan <rahul.maddy@gmail.com>
2021-02-09 19:37:48 +0530

    Readme

--- infiniteSum.py
+++ infiniteSum.py
@@ -9,6 +9,6 @@ def infiSumBetaPowerKminusiByKminusi(k=10,beta=0.5):
 
 if __name__ == "__main__":
     np.random.seed(8)
-    for k in range(0,100000001,100000000):
-        total = infiSumBetaPowerKminusiByKminusi(k,0.99999999)
-        print("(k,total)=(%d,%.7f)"%(k,total))
+    for k in range(0,10000001,10000000):
+        total = infiSumBetaPowerKminusiByKminusi(k,0.999)
+        print("(k,total)=(%d,%.3f)"%(k,total))
